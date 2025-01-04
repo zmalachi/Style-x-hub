4e15321aa41ca174ae18496353e71ced9f08f86a
malachi <121677370+zmalachi@users.noreply.github.com>
2025-01-04 15:48:44 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local PlaceId = game.PlaceId
 if PlaceId == 18668065416 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/zmalachi/Style-x-hub/refs/heads/main/blue-lock-rivals.lua")()
-end
+else
     game.Players.LocalPlayer:Kick("รอmalachi upนะสุดหล่อ")
 end
