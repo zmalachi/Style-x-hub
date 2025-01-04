bf819cf45a82eeb3b102baf75c6d0072ea09bc6e
malachi <121677370+zmalachi@users.noreply.github.com>
2025-01-04 15:40:50 +0700

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+local PlaceId = game.PlaceId
+if PlaceId == 18668065416 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/Build-A-Boat/main/README.md")()
+end
+    game.Players.LocalPlayer:Kick("รอmalachi upนะสุดหล่อ")
+end
