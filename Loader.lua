1bfdea6b404fb91862774260ddb64ccdbc5c1f87
malachi <121677370+zmalachi@users.noreply.github.com>
2025-01-04 15:42:08 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local PlaceId = game.PlaceId
 if PlaceId == 18668065416 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/Build-A-Boat/main/README.md")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/zmalachi/Style-x-hub/refs/heads/main/blue-lock-rivals.lua")()
 end
     game.Players.LocalPlayer:Kick("รอmalachi upนะสุดหล่อ")
 end
